ea44af6efa6bbe55359924fc821d3f187d9846d2
uditmahato <uditmahato29271@gmail.com>
2022-08-17 12:47:59 +0545

    revised design

--- base/urls.py
+++ base/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
-
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
