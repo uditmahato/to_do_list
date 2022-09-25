99ddf07f91923ad7aea9e8513c9f4bfe2e9e66ac
uditmahato <uditmahato29271@gmail.com>
2022-09-26 00:18:59 +0545

    added host

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd)=^c7!0-oqjmqve%(bt+p#sq6x*ipz2keh741j*-@f@_)f!1t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ssrcctodo.herokuapp.com']
 
 
 # Application definition
